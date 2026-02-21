51b67c47ab520c8d00aa0b89fa38a01db5fa174d
vc2mm4npwr-cyber <vc2mm4npwr@privaterelay.appleid.com>
2026-02-22 02:56:07 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const fadeElements = document.querySelectorAll('.fade-up');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.4
+});
+
+fadeElements.forEach(el => observer.observe(el));
